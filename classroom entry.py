ad65af59ba0c91f191a934fbc325e75b97959871
toxic123MMM <aashish02@gmail.com>
2024-10-21 17:20:41 +0400

    Add files via upload

---
+++ classroom entry.py
@@ -0,0 +1,8 @@
+age=int(input("Enter your age"))
+if age>=10:
+    if age<=20:
+        print("Your age is ",age,"  you are allowed to enter the class")
+    else:
+        print("Your age is ",age,"  you are  not allowed to enter the class")
+else:
+    print("Your age is ",age,"  you are not eligible to enter the class")
